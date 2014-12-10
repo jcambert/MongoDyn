fcea91ae42ca71fa99a33946c55ad7cca99aeee5
Ambert Jean-Christophe <j.ambert@free.fr>
2014-12-10 21:03:42 +0100

    1st commit

---
+++ MongoDynamic/DocumentSerializer.cs
@@ -0,0 +1,197 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDyn
+{
+    internal class DocumentSerializer : IBsonSerializer
+    {
+
+
+
+
+
+
+        public IBsonSerializationOptions GetDefaultSerializationOptions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize(MongoDB.Bson.IO.BsonWriter bsonWriter, Type nominalType, object value, IBsonSerializationOptions options)
+        {
+            if (nominalType == typeof(DocumentSerializer))
+                throw new ArgumentException("Cannot serialize anything but self");
+            Document document = value as Document;
+            var elts = document.Elements;
+            var propsDoc = document.BaseType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(p => p.CanWrite & p.GetCustomAttributes<KeyAttribute>().Count() == 0);
+            var propsType = nominalType.GetProperties(BindingFlags.Instance | BindingFlags.Public | System.Reflection.BindingFlags.DeclaredOnly).Where(p => p.CanWrite);
+            var key = document.BaseType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(p => p.CanWrite & p.GetCustomAttributes<KeyAttribute>().Count() == 1).FirstOrDefault();
+
+            bsonWriter.WriteStartDocument();
+
+            foreach (var prop in propsDoc)
+            {
+                
+                bsonWriter.WriteName(prop.Name);
+                BsonSerializer.Serialize(bsonWriter, prop.PropertyType, document[prop.Name]);
+            }
+
+
+            foreach (var prop in propsType)
+            {
+                if (!Dynamic.Audit && prop.Name.Equals("LastChanges")) continue;
+                bsonWriter.WriteName(prop.Name);
+                BsonSerializer.Serialize(bsonWriter, prop.PropertyType, prop.GetValue(value, null));
+            }
+
+            if (key != null)
+            {
+                bsonWriter.WriteName("_id");
+                BsonSerializer.Serialize(bsonWriter, key.PropertyType, document["_id"]);
+            }
+            bsonWriter.WriteEndDocument();
+
+        }
+
+
+        public bool GetDocumentId(out object id, out Type idNominalType, out IIdGenerator idGenerator)
+        {
+            id = null;
+            idGenerator = null;
+            idNominalType = typeof(Document);
+            return false;
+        }
+
+        /*  public void Serialize(MongoDB.Bson.IO.BsonWriter bsonWriter, Type nominalType, IBsonSerializationOptions options)
+          {
+              if (nominalType == typeof(DocumentSerializer))
+                  throw new ArgumentException("Cannot serialize anything but self");
+              ArraySerializer<Document> ser = new ArraySerializer<Document>();
+              ser.Serialize(bsonWriter,nominalType,value, options);
+          }
+          */
+        public void SetDocumentId(object id)
+        {
+            return;
+        }
+
+        public object Deserialize(MongoDB.Bson.IO.BsonReader bsonReader, Type nominalType, Type actualType, IBsonSerializationOptions options)
+        {
+            if (nominalType == typeof(DocumentSerializer)) throw new ArgumentException("Cannot deserialize anything but self");
+            var obj = Activator.CreateInstance(actualType);
+
+            bsonReader.ReadStartDocument();
+
+            while (bsonReader.ReadBsonType() != BsonType.EndOfDocument)
+            {
+                var name = bsonReader.ReadName();
+
+                var field = actualType.GetField(name);
+                if (field != null)
+                {
+                    var value = BsonSerializer.Deserialize(bsonReader, field.FieldType);
+                    field.SetValue(obj, value);
+                }
+
+                var prop = actualType.GetProperty(name);
+                if (prop != null)
+                {
+                    var value = BsonSerializer.Deserialize(bsonReader, prop.PropertyType);
+                    prop.SetValue(obj, value, null);
+                }
+            }
+
+            bsonReader.ReadEndDocument();
+
+            return obj;
+        }
+
+
+        public object Deserialize(MongoDB.Bson.IO.BsonReader bsonReader, Type nominalType, IBsonSerializationOptions options)
+        {
+            if (nominalType == typeof(DocumentSerializer)) throw new ArgumentException("Cannot deserialize anything but self");
+            Document obj = Activator.CreateInstance(nominalType) as Document;
+            object value=null;
+            bsonReader.ReadStartDocument();
+
+            while (bsonReader.ReadBsonType() != BsonType.EndOfDocument)
+            {
+                var name = bsonReader.ReadName();
+                var btype = bsonReader.CurrentBsonType;
+
+                if (!Dynamic.Audit && name.Equals("LastChanges")) continue;
+                
+                
+                switch (btype)
+                {
+                    case BsonType.Array:
+                        value = BsonSerializer.Deserialize(bsonReader,typeof(Array) );
+                        break;
+                    case BsonType.Binary:
+                        break;
+                    case BsonType.Boolean:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(Boolean));
+                        break;
+                    case BsonType.DateTime:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(DateTime));
+                        break;
+                    case BsonType.Document:
+                        break;
+                    case BsonType.Double:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(double));
+                        break;
+                    case BsonType.EndOfDocument:
+                        break;
+                    case BsonType.Int32:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(Int32));
+                        break;
+                    case BsonType.Int64:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(Int64));
+                        break;
+                    case BsonType.JavaScript:
+                        break;
+                    case BsonType.JavaScriptWithScope:
+                        break;
+                    case BsonType.MaxKey:
+                        break;
+                    case BsonType.MinKey:
+                        break;
+                    case BsonType.Null:
+                        break;
+                    case BsonType.ObjectId:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(Int32));
+                        break;
+                    case BsonType.RegularExpression:
+                        break;
+                    case BsonType.String:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(string));
+                        break;
+                    case BsonType.Symbol:
+                        break;
+                    case BsonType.Timestamp:
+                        value = BsonSerializer.Deserialize(bsonReader, typeof(DateTime));
+                        break;
+                    case BsonType.Undefined:
+                        break;
+                    default:
+                        break;
+                }
+
+                
+
+                obj[name] = BsonValue.Create(value);
+
+            }
+
+            bsonReader.ReadEndDocument();
+
+            return obj;
+        }
+    }
+}
---
+++ MongoDynamic/DynamicCollection.cs
@@ -0,0 +1,398 @@
+﻿using ImpromptuInterface;
+using ImpromptuInterface.Dynamic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDyn
+{
+    public sealed class DynamicCollection<TKey, TModel> : IDynamicCollection<TKey, TModel>
+       
+        where TModel : class
+    {
+
+        protected readonly bool NotifyEnabled;
+
+        internal readonly MongoCollection<Document> Collection;
+
+        /// <summary>
+        /// Cache property that will be used to eager load collections.
+        /// </summary>
+        internal MethodInfo CollectionQueryMethodInfo;
+
+        /// <summary>
+        /// Cache property that will be used to eager load foreignKeys
+        /// </summary>
+
+        internal MethodInfo GetByKeyMethodInfo;
+
+
+        protected readonly List<string> DocumentKeys = new List<string>();
+
+        internal readonly Dictionary<string, DynamicCollection<TKey, TModel>> FkCollections = new Dictionary<string, DynamicCollection<TKey, TModel>>();
+
+        internal readonly Dictionary<string, DynamicCollection<TKey, TModel>> ChildCollections = new Dictionary<string, DynamicCollection<TKey, TModel>>();
+
+        protected readonly PropertyInfo[] _properties = typeof(TModel).GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+        private bool _eagerLoad;
+
+        #region ctor
+
+        internal DynamicCollection(string collectionName, PropertyInfo key, bool notifyEnabled = false, bool audit = true)
+        {
+            Contract.Requires(collectionName != null && collectionName.Trim().Length > 0, "Collection Name must be non null nor empty");
+            Contract.Requires(key != null, "key must not be null");
+            Contract.Requires(typeof(TKey).Equals(key.PropertyType), "TKey and property info key must be the same type");
+            NotifyEnabled = notifyEnabled;
+            Collection = Dynamic.Db.GetCollection<Document>(collectionName);
+            Key = key;
+            KeyType = typeof(TKey);
+            Audit = audit;
+            initialize();
+        }
+
+        #endregion
+
+
+        private void initialize()
+        {
+            CollectionQueryMethodInfo = Helper.GetMethodInfo<DynamicCollection<TKey, TModel>>(x => x.CollectionQuery(null, 0, null));
+
+            GetByKeyMethodInfo = Helper.GetMethodInfo<DynamicCollection<TKey, TModel>>(x => x.GetByKey(default(TKey)));
+
+            _eagerLoad = Helper.IsEagerLoadEnabled<TModel>();
+
+            foreach (var propertyInfo in _properties/*.Where(propertyInfo => propertyInfo.CanWrite && propertyInfo.PropertyType.IsInterface)*/)
+            {
+                DocumentKeys.Add(propertyInfo.Name);
+            }
+
+            DocumentKeys.RemoveAll(p => p == Key.Name);
+
+            var indexes = Helper.GetIndexes<TModel>();
+
+            foreach (var tuple in indexes)
+            {
+                Collection.CreateIndex(tuple.Item1, tuple.Item2);
+            }
+        }
+
+        internal void PrepareEagerLoad()
+        {
+            if (!_eagerLoad)
+                return;
+
+            foreach (var propertyInfo in _properties.Where(p => p.CanWrite && p.PropertyType.IsInterface))
+            {
+                var type = propertyInfo.PropertyType;
+                if (type.IsGenericType)
+                {
+                    if (type.GetGenericTypeDefinition() == typeof(IEnumerable<>).GetGenericTypeDefinition())
+                    {
+                        var argu = type.GetGenericArguments().First();
+                        var repo = Dynamic.BuildRepository<TKey, TModel>(argu);
+                        ChildCollections.Add(argu.Name, repo);
+                    }
+                }
+                else
+                {
+                    var repo = Dynamic.BuildRepository<TKey, TModel>(type);
+                    FkCollections.Add(propertyInfo.Name, repo);
+                }
+            }
+        }
+
+        internal Document GetBsonDocumentById(object id)
+        {
+            Document dynamicDocument = Collection.FindOneById(BsonValue.Create(id));
+            return dynamicDocument ;
+        }
+
+        protected  void Update(object entity, object keyValue)
+        {
+            Document doc = BuildDocument(entity, keyValue);
+            Collection.Save(doc);
+        }
+
+        internal Document BuildDocument(object entity, object id)
+        {
+            var document = new Document(id, Audit);
+            document.BaseType = typeof(TModel);
+            foreach (var name in DocumentKeys)
+            {
+                var pValue = Impromptu.InvokeGet(entity, name);
+                try
+                {
+                    var bsonValue = BsonValue.Create(pValue);
+                    document[name] = bsonValue;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                    document[name] = BsonNull.Value;
+                    continue;
+                }
+            }
+
+            return document;
+        }
+
+        private TModel CastSingle(Document document)
+        {
+            if (document == null)
+                return null;
+
+            dynamic item = new ImpromptuDictionary(document.ToDictionary());
+            var keyValue = document.GetKey;
+            Impromptu.InvokeSetIndex(item, Key.Name, keyValue);
+
+            var entity = NotifyEnabled
+                       ? item.ActLike<TModel>(typeof(INotifyPropertyChanged))
+                       : item.ActLike<TModel>();
+
+            if (FkCollections.Count > 0)
+            {
+                TryGetFKs(entity);
+            }
+
+            if (ChildCollections.Count > 0)
+            {
+                TryGetChildCollections(entity);
+            }
+
+            return entity;
+        }
+
+        private IEnumerable<TModel> CastMany(IEnumerable<Document> documents)
+        {
+            return documents.Select(CastSingle).AsEnumerable();
+        }
+
+
+        void TryGetFKs(TModel entity)
+        {
+            foreach (var keyValuePair in FkCollections)
+            {
+                var qInfo = Helper.GetDefForFK<TModel>(keyValuePair.Key);
+
+                if (qInfo.SimpleProperty == null)
+                    continue;
+
+                var fkValue = Impromptu.InvokeGet(entity, qInfo.SimpleProperty);
+
+                if (fkValue == null)
+                    continue;
+
+                var mInfo = keyValuePair.Value.GetByKeyMethodInfo;
+
+                var result = mInfo.Invoke(keyValuePair.Value, new object[] { fkValue });
+                if (result == null)
+                    continue;
+
+                Impromptu.InvokeSet(entity, qInfo.ComplexPropertyName, result);
+            }
+        }
+
+        void TryGetChildCollections(TModel entity)
+        {
+            foreach (var keyValuePair in ChildCollections)
+            {
+                var queryCollectionInfo = Helper.GetDefForChildCollection<TModel>(keyValuePair.Key);
+
+                if (queryCollectionInfo.DetailTable == null)
+                    continue;
+
+                //id atual
+                var currentId = Impromptu.InvokeGet(entity, Key.Name);
+
+                var mInfo = keyValuePair.Value.CollectionQueryMethodInfo;
+                // GetMethod("CollectionQuery");
+
+                var result = mInfo.Invoke(keyValuePair.Value, new object[] { queryCollectionInfo.DetailKey, ExpressionType.Equal, currentId });
+
+                Impromptu.InvokeSet(entity, queryCollectionInfo.MasterProperty, result);
+            }
+        }
+
+        protected static bool IsDefaultValue(object keyValue)
+        {
+            if (keyValue == null)
+                return true;
+
+            var bsonValue = BsonValue.Create(keyValue);
+
+            switch (bsonValue.BsonType)
+            {
+                case BsonType.Int32:
+                    return Convert.ToInt32(keyValue) == 0;
+                default:
+                    return false;
+            }
+        }
+
+
+        private Document Insert(object entity)
+        {
+            Document newDocument = KeyType == typeof(int)
+                ? BuildDocument(entity, IdGenerator.GetNextIdFor(typeof(TModel)))
+                : BuildDocument(entity, null);
+
+            //newDocument.LastChanges = DateTime.Now;
+            Collection.Insert(newDocument);
+            return newDocument;
+        }
+
+        #region IDynamicCollection
+        public long Count
+        {
+            get { return Collection.Count(); }
+        }
+
+        public bool DeleteByKey(TKey key)
+        {
+            FindAndRemoveArgs fama = new FindAndRemoveArgs();
+            fama.Query = Query.EQ("_id", BsonValue.Create(key));
+            fama.SortBy = SortBy.Null;
+            var result = Collection.FindAndRemove(fama);
+            return result.Ok;
+        }
+
+        public TModel GetByKey(TKey key)
+        {
+            Document document =  GetBsonDocumentById(key);
+            var entity = document == null
+                       ? null
+                       : CastSingle(document);
+
+            return entity;
+        }
+
+        public TModel GetFirstOrDefault(System.Linq.Expressions.Expression<Func<TModel, bool>> id)
+        {
+            return id == null
+              ? CastSingle(Collection.FindOne())
+              : CustomQuery(id).FirstOrDefault();
+        }
+
+        public IEnumerable<TModel> All()
+        {
+            var documents = Collection.FindAllAs<Document>();
+            return CastMany(documents);
+        }
+
+        public IEnumerable<TModel> CollectionQuery(string memberName, System.Linq.Expressions.ExpressionType op, object value)
+        {
+            var bsonValue = BsonValue.Create(value);
+
+            IMongoQuery query = null;
+            switch (op)
+            {
+                case ExpressionType.Equal: query = Query.EQ(memberName, bsonValue);
+                    break;
+                case ExpressionType.GreaterThan:
+                    query = Query.GT(memberName, bsonValue);
+                    break;
+                case ExpressionType.GreaterThanOrEqual:
+                    query = Query.GTE(memberName, bsonValue);
+                    break;
+                case ExpressionType.LessThan:
+                    query = Query.LT(memberName, bsonValue);
+                    break;
+                case ExpressionType.LessThanOrEqual:
+                    query = Query.LTE(memberName, bsonValue);
+                    break;
+            }
+            return CastMany(Collection.FindAs<Document>(query));
+        }
+
+        public IEnumerable<TModel> CustomQuery(System.Linq.Expressions.Expression<Func<TModel, bool>> expression)
+        {
+            if (expression == null)
+                return All();
+
+            var query = QueryBuilder.BuildQuery(expression, Key.Name);
+
+            return query == null
+                ? Enumerable.Empty<TModel>()
+                : CastMany(Collection.FindAs<Document>(query));
+        }
+
+        public TModel New()
+        {
+            return NotifyEnabled
+                       ? new ImpromptuDictionary().ActLike<TModel>(typeof(INotifyPropertyChanged))
+                       : ImpromptuDictionary.Create<TModel>();
+        }
+
+        public void UpsertDynamic(IDictionary<string, object> item)
+        {
+            Upsert(item.ActLike<TModel>());
+        }
+
+        public void UpsertImpromptu(dynamic item)
+        {
+            Upsert(new ImpromptuDictionary(item).ActLike<TModel>());
+        }
+
+        public void Upsert(TModel item)
+        {
+            try
+            {
+                dynamic entity = item;
+                var keyValue = Impromptu.InvokeGet(entity, Key.Name);
+
+                if (IsDefaultValue(keyValue))
+                {
+                    Document document = Insert(entity);
+                    Impromptu.InvokeSet(item, Key.Name, document.GetKey);
+                }
+                else Update(entity, keyValue);
+            }
+            catch (MongoSafeModeException mongoSafeModeException)
+            {
+                Debug.WriteLine(mongoSafeModeException);
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception);
+                throw;
+            }
+        }
+
+        public bool Delete(TModel item)
+        {
+            var keyValue = Impromptu.InvokeGet(item, Key.Name);
+            return DeleteByKey(keyValue);
+        }
+
+        public void DeleteAll(bool resetCounter)
+        {
+            Collection.Drop();
+            if (resetCounter)
+            {
+                IdGenerator.ResetCounter<TModel>();
+            }
+        }
+        #endregion
+
+
+        #region Properties
+        protected PropertyInfo Key { get; private set; }
+
+        protected bool Audit { get; private set; }
+
+        protected Type KeyType { get; private set; }
+        #endregion
+    }
+}
