bfbd4fe2f9a64dde576348a39b3a79efb6291270
Ambert Jean-Christophe <j.ambert@free.fr>
2014-12-10 19:43:18 +0100

    ok

---
+++ MongoDynamic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDynamic
+{
+    public class Class1
+    {
+    }
+}
